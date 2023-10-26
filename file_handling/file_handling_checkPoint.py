4bbb8b0186bbc0766eb63f6666f8079af12b4454
burstman <hamedflissi@gmail.com>
2023-10-26 01:26:16 +0100

    adds some comments for clarification

--- file_handling/file_handling_checkPoint.py
+++ file_handling/file_handling_checkPoint.py
@@ -1,12 +1,18 @@
 import numpy as np
 # reading the csv file , (with) ensure the closing of the  file after reading
 with open("file_handling/Loan_prediction_dataset.csv", "r") as data:
+
+    # Skip the header row
     next(data)
+
+    # convert the eighth column to float and check if it's not empty. Empty cell discarded
+    # with if condition
     loan_amount = [float(line.strip().split(',')[8])
                    for line in data if line.strip().split(',')[8]]
 
 # convert data from csv tp ndarray object
 loan_amount_array = np.array(loan_amount)
+
 # perform  basic statistical analysis
 print("the mean of the loan amout:", np.mean(loan_amount_array))
 print("the median of loan amount:", np.median(loan_amount_array))
