3fee6266e02b2d74ac81256f2f288fd1dd6691a7
burstman <hamedflissi@gmail.com>
2023-12-23 05:03:55 +0100

    remove file

--- Artificial_neural_network/Artificial_Neural_Network_Checkpoint.py
+++ Artificial_neural_network/Artificial_Neural_Network_Checkpoint.py
@@ -1,1 +1,66 @@
-#empty file
+from __future__ import print_function
+
+import keras
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+(train_and_validation_images, train_and_validation_labels), (test_images,
+                                                             test_labels) = fashion_mnist.load_data()
+
+# Construct a validation set from the last 10000 images and labels from
+# train_and_validation_images and train_and_validation_labels
+validation_images = train_and_validation_images[-10000:, :, :]
+validation_labels = train_and_validation_labels[-10000:]
+
+# Construct a training set from the first 50000 images and labels.
+train_images = train_and_validation_images[:50000, :, :]
+train_labels = train_and_validation_labels[:50000]
+
+text_labels = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
+               'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+'''plt.figure()
+random_index = np.random.randint(0, len(train_images))
+plt.imshow(train_images[random_index], cmap='gray')
+plt.colorbar()
+numerical_label = train_labels[random_index]
+text_description = text_labels[numerical_label]
+plt.title('True Class: {} ("{}")'.format(numerical_label, text_description))
+
+plt.gca().grid(False)
+plt.show()'''
+
+'''plt.figure(figsize=(20,20))
+for i in range(50):
+    plt.subplot(5,10,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid('off') # type: ignore
+    
+    img_index = np.random.randint(0, 50000)
+    plt.imshow(train_images[img_index], cmap=plt.cm.gray) # type: ignore
+    plt.xlabel(text_labels[train_labels[img_index]])
+plt.show()'''
+
+    
+    # Flatten
+
+train_images_flatten = train_images.reshape(train_images.shape[0], 784)
+validation_images_flatten = validation_images.reshape(validation_images.shape[0], 784)
+test_images_flatten = test_images.reshape(test_images.shape[0], 784)
+
+# Normalize
+
+train_images_model = train_images_flatten / 255.0
+validation_images_model = validation_images_flatten / 255.0
+test_images_model = test_images_flatten / 255.0
+
+# convert class vectors to binary class matrices
+train_labels_categ = keras.utils.to_categorical(train_labels, 10)
+validation_labels_categ = keras.utils.to_categorical(validation_labels, 10)
+test_labels_categ = keras.utils.to_categorical(test_labels, 10)
